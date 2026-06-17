d0615b3697bf9b4f3e3db66109ac2de4c2a12869
Ubuntu <ubuntu@ip-172-31-26-121.ec2.internal>
2026-06-17 08:44:33 +0000

    Initial Terraform project

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "jenkins-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
